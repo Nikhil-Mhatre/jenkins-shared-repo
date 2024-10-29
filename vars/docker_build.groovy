8ec8d452152867c558f4906b32ae6d2729ac73c2
Nikhil Mhatre <144405614+Nikhil-Mhatre@users.noreply.github.com>
2024-10-29 11:27:54 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,5 @@
 def call(String PROJECT_NAME, String IMAGE_TAG, String DOCKER_HUB_USER){
+  echo "****************** Building the code ******************"
   sh "docker build -t ${DOCKER_HUB_USER}/${PROJECT_NAME}:${IMAGE_TAG} ."
+  echo "****************** Code Building Successfull ******************"
 }
